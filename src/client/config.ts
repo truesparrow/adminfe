624ce798c339a5846cb44c7d82dda92a52de6998
Horia Coman <horia141@gmail.com>
2018-03-17 17:28:17 +0200

    A refactoring of the configuration

--- src/client/config.ts
+++ src/client/config.ts
@@ -11,21 +11,32 @@ const clientConfigMarshaller = new (MarshalFrom(ClientConfig))();
 const clientConfig = clientConfigMarshaller.extract((window as any).__TRUESPARROW_CLIENT_CONFIG);
 delete (window as any).__TRUESPARROW_CLIENT_CONFIG;
 
+// Common to all services
 
-export const NAME: string = 'adminfe';
-export const ALLOWED_PATHS: PathMatch[] = clientConfig.allowedPaths;
-export const LOGOUT_ROUTE_PATH: string = clientConfig.logoutRoutePath;
 export const ENV: Env = clientConfig.env;
 export const CONTEXT: Context = Context.Client;
-export const ORIGIN: string = clientConfig.origin;
+
 export const SITEFE_EXTERNAL_HOST: string = clientConfig.siteFeExternalHost;
 export const CONTENT_SERVICE_HOST: string = clientConfig.contentServiceHost;
+
+export const FILESTACK_API_KEY: string = clientConfig.fileStackApiKey;
+export const ROLLBAR_CLIENT_TOKEN: string | null = clientConfig.rollbarClientToken;
+
+// Specific to adminfe service
+
+export const NAME: string = 'adminfe';
+export const ORIGIN: string = clientConfig.origin;
+
 export const AUTH0_CLIENT_CONFIG: Auth0ClientConfig = {
     clientId: clientConfig.auth0ClientId,
     domain: clientConfig.auth0Domain,
     loginCallbackUri: clientConfig.auth0LoginCallbackUri
 };
-export const FILESTACK_API_KEY: string = clientConfig.fileStackApiKey;
-export const ROLLBAR_CLIENT_TOKEN: string | null = clientConfig.rollbarClientToken;
+
+export const ALLOWED_PATHS: PathMatch[] = clientConfig.allowedPaths;
+export const LOGOUT_ROUTE_PATH: string = clientConfig.logoutRoutePath;
+
+// * Per-session "globals".
+
 export const SESSION: () => Session = () => clientConfig.session;
 export const LANG: () => string = () => clientConfig.language;
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -10,9 +10,38 @@ import {
     Session
 } from '@truesparrow/identity-sdk-js'
 
+// Common to all services
+
+export const ENV: Env = parseEnv(getFromEnv('COMMON_ENV'));
+export const CONTEXT: Context = parseContext(getFromEnv('COMMON_CONTEXT'));
+
+export const IDENTITY_SERVICE_HOST: string = getFromEnv('COMMON_IDENTITY_SERVICE_HOST');
+export const CONTENT_SERVICE_HOST: string = getFromEnv('COMMON_CONTENT_SERVICE_HOST');
+export const SITEFE_EXTERNAL_HOST: string = getFromEnv('COMMON_SITEFE_EXTERNAL_HOST');
+
+export const FILESTACK_API_KEY = getFromEnv('COMMON_FILESTACK_API_KEY');
+export const GOOGLE_MAPS_API_KEY = getFromEnv('COMMON_GOOGLE_MAPS_API_KEY');
+export const LOGGLY_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_LOGGLY_TOKEN') : null;
+export const LOGGLY_SUBDOMAIN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_LOGGLY_SUBDOMAIN') : null;
+export const ROLLBAR_SERVER_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_ROLLBAR_SERVER_TOKEN') : null;
+export const ROLLBAR_CLIENT_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_ROLLBAR_CLIENT_TOKEN') : null;
+
+// Specific to adminfe service
+
+export const NAME: string = 'adminfe';
+export const ADDRESS: string = getFromEnv('ADMINFE_ADDRESS');
+export const PORT: number = parseInt(getFromEnv('ADMINFE_PORT'), 10);
+export const ORIGIN: string = getFromEnv('ADMINFE_ORIGIN');
+
+export const AUTH0_SERVER_CONFIG: Auth0ServerConfig = {
+    clientId: getFromEnv('ADMINFE_AUTH0_CLIENT_ID'),
+    clientSecret: getFromEnv('ADMINFE_AUTH0_CLIENT_SECRET'),
+    domain: getFromEnv('ADMINFE_AUTH0_DOMAIN'),
+    loginCallbackUri: getFromEnv('ADMINFE_AUTH0_LOGIN_CALLBACK_URI')
+};
+export const AUTH0_CLIENT_CONFIG: Auth0ClientConfig = serverToClient(AUTH0_SERVER_CONFIG);
 
 export const CLS_NAMESPACE_NAME: string = 'truesparrow.request';
-export const NAME: string = 'adminfe';
 export const ALLOWED_PATHS: PathMatch[] = [
     { path: '/', mode: 'full' },
     { path: '/admin', mode: 'full' },
@@ -20,27 +49,8 @@ export const ALLOWED_PATHS: PathMatch[] = [
 ];
 export const LOGOUT_ROUTE_PATH: string = '/real/auth0-auth-flow/logout';
 
-export const ENV: Env = parseEnv(getFromEnv('ENV'));
-export const CONTEXT: Context = parseContext(getFromEnv('CONTEXT'));
-export const ADDRESS: string = getFromEnv('ADDRESS');
-export const PORT: number = parseInt(getFromEnv('PORT'), 10);
-export const ORIGIN: string = getFromEnv('ORIGIN');
-export const SITEFE_EXTERNAL_HOST: string = getFromEnv('SITEFE_EXTERNAL_HOST');
-export const IDENTITY_SERVICE_HOST: string = getFromEnv('IDENTITY_SERVICE_HOST');
-export const CONTENT_SERVICE_HOST: string = getFromEnv('CONTENT_SERVICE_HOST');
-export const AUTH0_SERVER_CONFIG: Auth0ServerConfig = {
-    clientId: getFromEnv('AUTH0_CLIENT_ID'),
-    clientSecret: getFromEnv('AUTH0_CLIENT_SECRET'),
-    domain: getFromEnv('AUTH0_DOMAIN'),
-    loginCallbackUri: getFromEnv('AUTH0_LOGIN_CALLBACK_URI')
-};
-export const AUTH0_CLIENT_CONFIG: Auth0ClientConfig = serverToClient(AUTH0_SERVER_CONFIG);
-export const FILESTACK_API_KEY = getFromEnv('FILESTACK_API_KEY');
-export const GOOGLE_MAPS_API_KEY = getFromEnv('GOOGLE_MAPS_API_KEY');
-export const LOGGLY_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('LOGGLY_TOKEN') : null;
-export const LOGGLY_SUBDOMAIN: string | null = isOnServer(ENV) ? getFromEnv('LOGGLY_SUBDOMAIN') : null;
-export const ROLLBAR_SERVER_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('ROLLBAR_SERVER_TOKEN') : null;
-export const ROLLBAR_CLIENT_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('ROLLBAR_CLIENT_TOKEN') : null;
+// * Per-session "globals".
+
 export const SESSION: () => Session = () => {
     const namespace = getNamespace(CLS_NAMESPACE_NAME);
     const session = namespace.get('SESSION');
