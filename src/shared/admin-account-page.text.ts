ac3180791c21344405788488f9046372d9c6fbc5
Horia Coman <horia141@gmail.com>
2018-04-29 08:52:18 +0300

    Add a title and description to the account page

--- src/shared/admin-account-page.text.ts
+++ src/shared/admin-account-page.text.ts
@@ -1,5 +1,15 @@
-import { Message } from '@truesparrow/common-js'
+import { Message, MessageWith0Arg } from '@truesparrow/common-js'
 
+
+export const pageTitle: Message = {
+    en: 'TruSpar - Account',
+    ro: 'TruSpar - Cont'
+};
+
+export const pageDescription: MessageWith0Arg = {
+    en: 'Account administration page',
+    ro: 'Pagina de administrare a contului'
+};
 
 export const adminAccountPage: Message = {
     en: 'Account page',
--- src/shared/admin-account-page.tsx
+++ src/shared/admin-account-page.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react'
+import { Helmet } from 'react-helmet'
 
 import * as config from './config'
 
 import * as text from './admin-account-page.text'
 
 
-export const AdminAccountPage = () => <div>{text.adminAccountPage[config.LANG()]}</div>;
+export const AdminAccountPage = () =>
+    <div>
+        <Helmet>
+            <title>{text.pageTitle[config.LANG()]}</title>
+            <meta name="description" content={text.pageDescription[config.LANG()]} />
+        </Helmet>
+        {text.adminAccountPage[config.LANG()]}
+    </div>;
