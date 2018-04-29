471f4add7d0576c1d1116ed827ad447f167b0754
Horia Coman <horia141@gmail.com>
2018-04-29 14:51:43 +0300

    Added title and description for the not found page

--- src/shared/not-found-page.text.ts
+++ src/shared/not-found-page.text.ts
@@ -1,5 +1,17 @@
-import { Message } from '@truesparrow/common-js'
+import { Message, MessageWith0Arg } from '@truesparrow/common-js'
 
+import * as config from './config'
+
+
+export const pageTitle: Message = {
+    en: `${config.STYLE_APPLICATION_NAME} - Page Not Found`,
+    ro: `${config.STYLE_APPLICATION_NAME} - Pagina Nu Există`
+};
+
+export const pageDescription: MessageWith0Arg = {
+    en: 'Page not found',
+    ro: 'Pagină nu există'
+};
 
 export const notFound: Message = {
     en: 'Could not find the page',
---
+++ src/tests/not-found.ts
@@ -0,0 +1,18 @@
+import 'mocha'
+
+describe.only('Not found page', () => {
+    before(() => {
+        cy.clearOutData();
+    });
+
+    it('Should be a scaffold', () => {
+        cy.visit('/inexistent-page', { failOnStatusCode: false });
+        cy.get('main').contains('Could not find the page');
+    });
+
+    it('Web integration', () => {
+        cy.visit('/inexistent-page', { failOnStatusCode: false });
+        cy.title().should('equal', 'TruSpar - Page Not Found');
+        cy.get('head > meta[name=description]').should('have.attr', 'content', 'Page not found');
+    })
+});
